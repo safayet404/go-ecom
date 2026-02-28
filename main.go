fb2c7914f17e202a579c6c3a6c837713accf6358
safayet404 <hossainsafayet187@gmail.com>
2026-03-01 02:07:47 +0600

    product list:

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -16,11 +17,34 @@ func aboutHadnler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "hello this is afayet")
 }
 
+type Product struct {
+	ID          int
+	Title       string
+	Description string
+	Price       float64
+	ImgUrl      string
+}
+
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Please give me GET request", 400)
+
+		return
+	}
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(productList)
+
+}
+
+var productList []Product
+
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", helloHandler)
 	mux.HandleFunc("/about", aboutHadnler)
+	mux.HandleFunc("/products", getProducts)
 
 	fmt.Println("Server running on Server : 3000")
 
@@ -30,4 +54,33 @@ func main() {
 		fmt.Println("error in staring the server", err)
 	}
 
+}
+
+func init() {
+	prd1 := Product{
+		ID:          1,
+		Title:       "Orange",
+		Description: "This is orage fruit",
+		Price:       123.45,
+		ImgUrl:      "dole.com/sites/default/files/mdedia/2025-01/orange.png",
+	}
+
+	prd2 := Product{
+		ID:          2,
+		Title:       "Apple",
+		Description: "This is orage fruit",
+		Price:       123.45,
+		ImgUrl:      "dole.com/sites/default/files/mdedia/2025-01/orange.png",
+	}
+	prd3 := Product{
+		ID:          3,
+		Title:       "Banana",
+		Description: "This is orage fruit",
+		Price:       123.45,
+		ImgUrl:      "dole.com/sites/default/files/mdedia/2025-01/orange.png",
+	}
+
+	productList = append(productList, prd1)
+	productList = append(productList, prd2)
+	productList = append(productList, prd3)
 }
