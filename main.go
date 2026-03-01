ef4bfa3d87bcbb60025e82df5bd43e5127ca00e2
safayet404 <hossainsafayet187@gmail.com>
2026-03-01 13:48:40 +0600

    Header set

--- main.go
+++ main.go
@@ -26,6 +26,8 @@ type Product struct {
 }
 
 func getProducts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
 	if r.Method != http.MethodGet {
 		http.Error(w, "Please give me GET request", 400)
 
