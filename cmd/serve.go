5807ab67a630448de50230ce3715dd4cdb40ffbd
safayet404 <hossainsafayet187@gmail.com>
2026-03-08 23:13:40 +0600

    db connected

--- cmd/serve.go
+++ cmd/serve.go
@@ -2,15 +2,27 @@ package cmd
 
 import (
 	"ecommerce/config"
+	"ecommerce/infra/db"
 	"ecommerce/rest"
 	"ecommerce/rest/handlers/product"
 	"ecommerce/rest/handlers/user"
 	middleware "ecommerce/rest/middlewares"
+	"fmt"
+	"os"
 )
 
 func Serve() {
 
 	cnf := config.GetConfig()
+
+	dbCon, err := db.NewConnection()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(dbCon)
 
 	middlewares := middleware.NewMiddlewares(cnf)
 	productHandler := product.NewHandler(middlewares)
--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module ecommerce
 go 1.24.0
 
 require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/jmoiron/sqlx v1.4.0 // indirect
+	github.com/lib/pq v1.11.2 // indirect
+)
